95b85c7954b20b29c201c3b19720143fb7fd1f03
Keith Jung <60eokk@gmail.com>
2024-05-28 17:16:15 -0500

    import pytorch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("hi")
-print('hi2')
-print('hi5')
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
